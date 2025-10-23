903875be0b7c95b5e98d4a0a9e0f95f503ae8142
Md Selim <mdselim.ibn@gmail.com>
2025-10-23 17:19:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1610,12 +1610,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 });
-
-// Lazy load service worker registration
-if ('serviceWorker' in navigator && !sessionStorage.getItem('sw-registered')) {
-    setTimeout(() => {
-        navigator.serviceWorker.register('/sw.js', { scope: '/' })
-            .then(() => sessionStorage.setItem('sw-registered', 'true'))
-            .catch(err => console.log('SW registration failed:', err));
-    }, 3000);
-}
