6a2f894cde717ccdb38f2fad965e762900ee56fd
Md Selim <mdselim.ibn@gmail.com>
2025-10-25 13:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://perspicacitybackend.onrender.com';
+const API_BASE_URL = 'https://perspicacity-backend.onrender.com';
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
